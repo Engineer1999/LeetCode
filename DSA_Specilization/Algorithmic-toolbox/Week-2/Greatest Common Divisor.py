b2fb8d3c4ce5a3ab059cd61e5574cf43e462151d
Engineer1999 <callbhargavp@gmail.com>
2022-01-29 20:12:58 +0530

    added 3 assignments in week-2

---
+++ DSA_Specilization/Algorithmic-toolbox/Week-2/Greatest Common Divisor.py
@@ -0,0 +1,22 @@
+def GCD(numbers):
+    
+    if numbers[0]>numbers[1]:
+        a, b = numbers[0], numbers[1]
+    else:
+        a, b = numbers[1], numbers[0]
+
+    
+    while a*b!=0:
+        b, a = a%b, b
+    if a==0:
+        return b
+    else:
+        return a
+            
+
+if __name__ == '__main__':
+    input_numbers = [int(x) for x in input().split()]
+    ans = GCD(input_numbers)
+    print(ans)
+    
+        
---
+++ DSA_Specilization/Algorithmic-toolbox/Week-2/Last Digit of a Large Fibonacci Number.py
@@ -0,0 +1,17 @@
+def FibNum(nums):
+    ans = []
+    ans.append(int(0))
+    ans.append(int(1))
+    for i in range(nums):
+        ans.append(int(ans[i] + ans[i+1]))
+    
+    return ans[len(ans)-2]%10
+        
+    
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    ans = FibNum(input_n)
+    print(ans)
+    
