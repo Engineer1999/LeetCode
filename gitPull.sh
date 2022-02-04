a21db8b1272832d7137b13c0a93fe52cafafbb4d
Engineer1999 <callbhargavp@gmail.com>
2022-02-04 22:01:17 +0530

    s

--- gitPull.sh
+++ gitPull.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+GIT=`which git`
+echo $GIT
 echo "Enter the commit message: "
 read commit_msg
-git status
-git add *
-git commit -m $commit_msg
-git push
+${GIT} status
+${GIT} add --all
+${GIT} commit -m $commit_msg
+${GIT} push
