f46be6dad85d8d18142048f19445424ddb33a78e
Engineer1999 <callbhargavp@gmail.com>
2022-02-04 22:07:52 +0530

    modified_git_push_automation

--- gitPull.sh
+++ gitPull.sh
@@ -3,8 +3,14 @@
 GIT=`which git`
 echo $GIT
 echo "Enter the commit message: "
-read commit_msg
+read -r commit_msg
 ${GIT} status
+echo "-------------------------------------"
 ${GIT} add --all
+echo "-------------------------------------"
+${GIT} status
+echo "-------------------------------------"
 ${GIT} commit -m $commit_msg
+echo "-------------------------------------"
 ${GIT} push
+echo "-------------------------------------"
